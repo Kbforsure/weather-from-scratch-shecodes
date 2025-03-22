d809e8435ab44d8a13a22bc0358a5239877a142a
kbforsure <katieannablair@gmail.com>
2025-03-22 16:33:19 -0700

    testing js in form element

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+function handleSearchSumbit(event) {
+  event.preventdefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
 let SearchFormElement = document.querySelector("#search-form");
-console.log(SearchFormElement);
+SearchFormElement.addEventListener("submit", handleSearchSumbit);
