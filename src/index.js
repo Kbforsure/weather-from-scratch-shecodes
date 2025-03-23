1893884c00de6eab644329b5979dc8d8f4376e6a
kbforsure <katieannablair@gmail.com>
2025-03-22 17:54:28 -0700

    testing api functions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+function refreshWeather(response) {
+  console.log(response.data);
+}
+
 function searchCity(city) {
   let apiKey = "do8e05a55a39a74t37328f1154eb845f";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
