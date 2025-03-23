46684ec048ff4d03dde9c8d9917269854ad6f711
kbforsure <katieannablair@gmail.com>
2025-03-22 18:02:42 -0700

    testing API link to id

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
   console.log(response.data.temperature.current);
 }
 
