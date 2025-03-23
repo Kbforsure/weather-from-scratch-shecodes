251b176b6215a32492ac623e17ffda9de10db67f
kbforsure <katieannablair@gmail.com>
2025-03-22 17:06:21 -0700

    found weird mistake

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-function handleSearchSumbit(event) {
-  event.preventdefault();
+function handleSearchSubmit(event) {
+  event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("h1");
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSumbit);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
