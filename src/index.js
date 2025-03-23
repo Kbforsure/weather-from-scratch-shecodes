65f669c90a3c12e737a726adb78a15887c984ea9
kbforsure <katieannablair@gmail.com>
2025-03-22 17:48:59 -0700

    adding api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+function searchCity(city) {
+  let apiKey = "do8e05a55a39a74t37328f1154eb845f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector(".search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
