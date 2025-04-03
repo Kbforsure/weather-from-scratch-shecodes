412f0d80751096af2b3d07e1e6dbbafa615e18c1
kbforsure <katieannablair@gmail.com>
2025-04-02 17:01:09 -0700

    formatting forecasted days

--- src/index.js
+++ src/index.js
@@ -51,6 +51,20 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return days[date.getDay()];
+}
 function getForecast(city) {
   let apiKey = "do8e05a55a39a74t37328f1154eb845f";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -66,7 +80,7 @@ function displayForecast(response) {
       forecastHtml =
         forecastHtml +
         `<div class="weather-forecast-day">
-            <div class="weather-forecast-date">Tue</div>
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
                 <img src="${
                   day.condition.icon_url
                 }" class="weather-forecast-icon" />
